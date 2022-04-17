e84484f87fff9d57b726fa796c9831fae7ecb850
brennamehl <brennam@email.unc.edu>
2022-04-17 15:30:48 -0400

    changed quotes from single to double

--- database.js
+++ database.js
@@ -2,8 +2,7 @@ const database = require("better-sqlite3")
 const logdb = new database("log.db");
 
 //sets up new database, SQL syntax in ticks
-const stmt = logdb.prepare(`SELECT name FROM sqlite_master 
-                            WHERE type=‘table’ and name=‘accesslog’;`) 
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type="table" and name="accesslog";`) 
 //get function returns first row
 let row = stmt.get(); 
 if(row===undefined){
